f0afd2330b35201a431a9c3e374f9420fa25ae1c
no115 <no11519no@gmail.com>
2026-05-22 15:14:29 +0900

    chore: prune legacy translator interpreter UI

--- src/app/components/home-translator/interpreter/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as InShopInterpreterMvp } from "./InShopInterpreterMvp";
-export { ConversationHistoryList } from "./ConversationHistoryList";
-export { InterpreterHeader } from "./InterpreterHeader";
-export { LanguageSelectorRow } from "./LanguageSelectorRow";
-export { QuickPhrasePanel } from "./QuickPhrasePanel";
-export { SpeakerControlCard } from "./SpeakerControlCard";
-export { buildInterpreterHistoryKey, mergeInterpreterHistory, parseInterpreterHistory } from "./storage";
-export { useInterpreterHistory } from "./hooks/useInterpreterHistory";
-export { usePushToTalkRecorder } from "./hooks/usePushToTalkRecorder";
